116fd49ef7d43a6f7f886c53c40785e33ab3f365
valentin199815 <valentinteruggi@gmail.com>
2022-04-03 23:11:26 -0700

    final commit

--- src/components/joblist.js
+++ src/components/joblist.js
@@ -90,24 +90,29 @@ function Joblist() {
                   jobs.reduce(j=>{
                       return(
                         <>
-                        
                           {
                             data.map(item =>{
+                              jobs.map(job => {
                               if(item.categories.location == searchbylocation){
-                                return(
-                                  <>
-                                    <h2 className='grouped'>{item.categories.team}</h2>
-                                    <div key={item.id} className="jobs">
-                                        <div className='data'>
-                                          <h2>{ item.text }</h2> 
-                                          <p id='location'>{item.categories.location} / <span>{item.categories.team}</span> </p>
+                                  if(item.categories.team == job){
+                                    
+                                    return(
+                                      <>
+                                        
+                                        <div key={item.id} className="jobs">
+                                            <div className='data'>
+                                              <h2>{ item.text }</h2> 
+                                              <p id='location'>{item.categories.location} / <span>{item.categories.team}</span> </p>
+                                            </div>
+                                        <div><a type="button" target="_blank" href={"https://paralleldomain.com/job?id="+item.id}>Apply</a></div>
+                                          
                                         </div>
-                                    <div><a type="button" target="_blank" href={"https://paralleldomain.com/job?id="+item.id}>Apply</a></div>
-                                      
-                                    </div>
-                                </>
-                                )
+                                    </>
+                                    )
+                                                                  
                               }
+                            }
+                            })
                             })
                           }
                         </>
