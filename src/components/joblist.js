0650cc3cec69ef309233cbd8794d85b581b03103
valentin199815 <valentinteruggi@gmail.com>
2022-04-03 22:55:34 -0700

    final commit

--- src/components/joblist.js
+++ src/components/joblist.js
@@ -11,6 +11,10 @@ function Joblist() {
   const [searchbytime, setSearchTime] = useState("");
   const [loading, setLoading] = useState(true);
 
+  var jobs = ["Computer Graphics Content","Engineering","Strategy & Operations","Product Engineering",
+  "People","Go-To-Market","Think You Fit Somewhere Else?"];
+  var locations = ["San Francisco Bay Area","Vancouver, British Columbia, Canada"]
+
   useEffect(() =>{
     axios
       .get("https://api.lever.co/v0/postings/paralleldomain?mode=json")
@@ -22,18 +26,6 @@ function Joblist() {
           console.log(err)
         })
   }, [])
-
-  const filterjobs = data.filter(item =>{
-      if(item.categories.location.includes(searchbylocation) && item.categories.team.includes(searchbyTeam) && item.categories.team.includes(searchbytime)){
-        return item.categories.location.includes(searchbylocation);
-      }
-      
-      
-  });
-
-  var jobs = ["Computer Graphics Content","Engineering","Strategy & Operations","Product Engineering",
-  "People","Go-To-Market","Think You Fit Somewhere Else?"];
-
   
   if(loading) return <h1><CircularProgress/></h1>;
 
@@ -42,22 +34,20 @@ function Joblist() {
           <div className='filters'>
             <select onChange={(e)=> setSearchTeam(e.target.value)}>
               <option value="" onClick={(e)=>setSearchTeam(e.target.value)}>No preferred</option>
-              <option value="Computer Graphics Content" onClick={(e)=>setSearchTeam(e.target.value)}>Computer Graphics Content</option>
-              <option value="Engineering" onClick={(e)=>setSearchTeam(e.target.value)}>Engineering</option>
-              <option value="Strategy & Operations" onClick={(e)=>setSearchTeam(e.target.value)}>Strategy & Operations</option>
-              <option value="Product Engineering" onClick={(e)=>setSearchTeam(e.target.value)}>Product Engineering  </option>
-              <option value="People" onClick={(e)=>setSearchTeam(e.target.value)}>People</option>
-              <option value="Go-To-Market" onClick={(e)=>setSearchTeam(e.target.value)}>Go-To-Market</option>
-              <option value="Think You Fit Somewhere Else?" onClick={(e)=>setSearchTeam(e.target.value)}>I don't know yet</option>
+              {jobs.map(job => {
+                return <option value={job} onClick={(e)=>setSearchTeam(e.target.value)}>{job}</option>
+              })}
             </select>
 
             <select onChange={(e)=> setSearchLocation(e.target.value)}>
-            <option value="" onClick={(e)=> setSearchLocation(e.target.value)}>No preferred</option>
-              <option value="San Francisco Bay Area" onClick={(e)=> setSearchLocation(e.target.value)}>San Francisco Bay Area</option>
-              <option value="Vancouver, British Columbia, Canada" onClick={(e)=> setSearchLocation(e.target.value)}>Vancouver, British Columbia, Canada</option>
+              <option value="" onClick={(e)=> setSearchLocation(e.target.value)}>No preferred</option>
+              {locations.map(location => {
+                return <option value={location} onClick={(e)=>setSearchLocation(e.target.value)}>{location}</option>
+              })}
+              
             </select>
             <select>
-              <option value="fulltime" onClick={(e)=> setSearchTime(e.target.value)}>No election</option>
+              <option value="" onClick={(e)=> setSearchTime(e.target.value)}>No election</option>
               <option value="fulltime" onClick={(e)=> searchbytime(e.target.value)}>Full time</option>
             </select>
             
@@ -67,7 +57,7 @@ function Joblist() {
           {
             
             jobs.reduce(i=>{
-              if(searchbyTeam == ""){
+              if(searchbyTeam == "" && searchbylocation ==""){
                 return(
                 jobs.map(j=>{
                     return(
@@ -94,6 +84,36 @@ function Joblist() {
                       </>
                     )
               }))
+              }
+              else if(searchbyTeam == "" && searchbylocation != ""){
+                return(
+                  jobs.reduce(j=>{
+                      return(
+                        <>
+                        
+                          {
+                            data.map(item =>{
+                              if(item.categories.location == searchbylocation){
+                                return(
+                                  <>
+                                    <h2 className='grouped'>{item.categories.team}</h2>
+                                    <div key={item.id} className="jobs">
+                                        <div className='data'>
+                                          <h2>{ item.text }</h2> 
+                                          <p id='location'>{item.categories.location} / <span>{item.categories.team}</span> </p>
+                                        </div>
+                                    <div><a type="button" target="_blank" href={"https://paralleldomain.com/job?id="+item.id}>Apply</a></div>
+                                      
+                                    </div>
+                                </>
+                                )
+                              }
+                            })
+                          }
+                        </>
+                      )
+                }))   
+                
               }else{
                 return(
                   <>
